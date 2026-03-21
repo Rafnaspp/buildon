b8b49dc2d870fb3c8d3d3c3ab5be63f4e9318701
sj-ms-sudo <mashoodsajad@gmail.com>
2026-03-21 13:37:32 +0530

    Fixed the responsiveness of client review and fixed the metadata icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,8 +7,16 @@ const inter = Inter({ subsets: ['latin'] })
 export const metadata: Metadata = {
   title: 'BUILDON',
   description: "BUILDON Building Materials Trading LLC supplies high-quality construction materials, safety products, fasteners, and industrial tools trusted by contractors, engineers, and project developers across the UAE .With a growing network of suppliers and partners across the UAE and international markets, BUILDON provides reliable access to essential construction materials, safety equipment, tools, and industrial hardware for projects of every scale.",
-  icons: {
-    icon: "/buildon.png"
+  icons :{
+    icon :[
+      {url:"/buildon.png",sizes:"32x32",type:"image/png"}
+      // {url:"/icons/favicon.ico"},
+      // {url:"/icons/favicon-16x16.png",sizes:"16x16",type:"image/png"},
+      // {url:"/icons/favicon-32x32.png",sizes:"32x32",type:"image/png"}
+    ],
+    // apple:[
+    //   {url:"icons/apple-touch-icon.png",sizes:"180x180",type:"image/png"}
+    // ],
   }
 }
 
